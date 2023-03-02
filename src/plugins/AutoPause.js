be7beb5a95a1be2ab3c7fabcb74e2f3e3972a72d
ghalliard <gabrielmontoyahuamani26@gmail.com>
2023-03-01 19:53:30 -0500

    clase 22 terminada

--- src/plugins/AutoPause.js
+++ src/plugins/AutoPause.js
@@ -1,6 +1,7 @@
 class AutoPause{
     constructor(){
         this.handlerIntersection = handlerIntersection_fnc.bind(this);
+        this.handlerVisibilityChange = handlerVisibilityChange_fnc.bind(this);
         this.threshold = 0.25;
         this.options = {
             threshold: this.threshold,
@@ -10,12 +11,17 @@ class AutoPause{
         this.player = player;
         const observer = new IntersectionObserver(this.handlerIntersection, this.options);
         observer.observe(this.player.media);
+
+        // añadimos el visibility change
+        document.addEventListener('visibilitychange', this.handlerVisibilityChange);
     }
 }
-
+function handlerVisibilityChange_fnc(){
+    const isVisible = document.visibilityState === 'visible';
+    isVisible ? this.player.play() : this.player.pause();
+}
 function handlerIntersection_fnc(entries){
     // en este caso cuando usamos this dentro de esta funcion, va a señalar a Intersection observer. Por ese motivo en el constructor se estaría usando la funcion bind.
-    console.log(this);
     const entry = entries[0];
     if(entry.intersectionRatio < this.threshold){
         this.player.pause();
